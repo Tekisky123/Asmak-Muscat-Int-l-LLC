382efaf34865a2d284d0796338215e83a4371677
suleman <sullukhan123786@gmail.com>
2024-11-25 13:22:38 +0530

    updated product

--- src/Components/FooterSection.jsx
+++ src/Components/FooterSection.jsx
@@ -6,116 +6,67 @@ import {
   FaLinkedinIn,
   FaTwitter,
 } from "react-icons/fa";
+import "../assets/Styles/Footer.css";
 
 const FooterSection = () => {
   return (
-    <footer className="footer section  text-center">
-      <div className="">
-        <div className=" grid-list">
-          <div className="footer-brand has-before has-after">
-            <Link to="/" className="logo">
-              <img
-                src={logo}
-                width="260"
-                height="50"
-                loading="lazy"
-                alt="Asmak Muscat Intl Logo"
-              />
-            </Link>
-
-            <address className="body-4">
-              P.O.BOX 1741, POSTAL CODE 122, AL MABELA, SULTANATE OF OMAN
-            </address>
-
-            <Link to="mailto:info@asmakmct.com" className="body-4 contact-link">
-              info@asmakmct.com
-            </Link>
-
-            <p className="body-4 contact-link">Mobile: +968 92850600</p>
-            <p className="body-4 contact-link">Tel: +968 24050604</p>
-          </div>
-
-          <ul className="footer-list">
-            <li>
-              <Link to="/" className="label-2 footer-link hover-underline">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about-us"
-                className="label-2 footer-link hover-underline"
-              >
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/products"
-                className="label-2 footer-link hover-underline"
-              >
-                Products
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                className="label-2 footer-link hover-underline"
-              >
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-
-          <ul className=" icon-list">
-            <li>
-              <Link
-                to="https://twitter.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="label-2 footer-link hover-underline"
-              >
-                <FaTwitter className="icon" /> 
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="https://facebook.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="label-2 footer-link hover-underline"
-              >
-                <FaFacebookF className="icon" /> 
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="https://plus.google.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="label-2 footer-link hover-underline"
-              >
-                <FaGooglePlusG className="icon" /> 
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="https://linkedin.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="label-2 footer-link hover-underline"
-              >
-                <FaLinkedinIn className="icon" /> 
-              </Link>
-            </li>
-          </ul>
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-brand">
+          <Link to="/" className="footer-logo">
+            <img src={logo} alt="Asmak Muscat Intl Logo" className="footer-logo-img" />
+          </Link>
+          <address className="footer-address">
+            P.O.BOX 1741, POSTAL CODE 122, AL MABELA, SULTANATE OF OMAN
+          </address>
+          <Link to="mailto:info@asmakmct.com" className="footer-email">
+            info@asmakmct.com
+          </Link>
+          <p className="footer-contact">Mobile: +968 92850600</p>
+          <p className="footer-contact">Tel: +968 24050604</p>
         </div>
 
-        <div className="footer-bottom">
-          <p className="copyright">
-            &copy; 2019 Asmak Muscat Int&apos;l. All Rights Reserved
-          </p>
-        </div>
+        <ul className="footer-links">
+          <li>
+            <Link to="/" className="footer-link">Home</Link>
+          </li>
+          <li>
+            <Link to="/about-us" className="footer-link">About Us</Link>
+          </li>
+          <li>
+            <Link to="/products" className="footer-link">Products</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="footer-link">Contact Us</Link>
+          </li>
+        </ul>
+
+        <ul className="footer-socials">
+          <li>
+            <Link to="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaTwitter />
+            </Link>
+          </li>
+          <li>
+            <Link to="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaFacebookF />
+            </Link>
+          </li>
+          <li>
+            <Link to="https://plus.google.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaGooglePlusG />
+            </Link>
+          </li>
+          <li>
+            <Link to="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FaLinkedinIn />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer-bottom">
+        <p className="footer-copy">&copy; 2019 Asmak Muscat Int&apos;l. All Rights Reserved</p>
       </div>
     </footer>
   );
--- src/Components/MenuSection.jsx
+++ src/Components/MenuSection.jsx
@@ -1,30 +1,26 @@
 import { Link } from "react-router-dom";
-import menu1 from "../assets/images/BlueLineSnapper.jpg";
-import menu2 from "../assets/images/Cuttlefish.jpg";
-import menu3 from "../assets/images/Emperor.jpg";
-import menu4 from "../assets/images/grouper.jpeg";
-import menu5 from "../assets/images/Soldier.jpg";
-import menu6 from "../assets/images/Yellowfin.avif";
-import menu7 from "../assets/images/Panulirus.jpeg";
-import menu8 from "../assets/images/Trichiurus.jpg";
-import menu9 from "../assets/images/Sardinella.jpeg";
-import menu10 from "../assets/images/Penaeus.jpg";
-import menu11 from "../assets/images/Carangoides.png";
-import menu12 from "../assets/images/Epinephelus.jpg";
+import yellowfinImage from "../assets/images/yellowfinn.avif";
+import kawakawaImage from "../assets/images/Kawa-Kawa-Tuna-scaled.jpg";
+import bonitoImage from "../assets/images/bonitol-web.jpg";
+import emperorImage from "../assets/images/Emperor.jpg";
+import arabianPandoraImage from "../assets/images/arabian.jpg";
+import redSeabreamImage from "../assets/images/fish-red-bream.jpg";
+import trevallyImage from "../assets/images/travelly.jpg";
+import kofferImage from "../assets/images/koffer.jpg";
+import threadfinBreamImage from "../assets/images/treadfin.jpg";
+import blueLineSnapperImage from "../assets/images/blueline.jpg";
 
 const productData = [
-  { id: 13, name: "Blue Line Snapper", image: menu1 },
-  { id: 14, name: "Cuttlefish", image: menu2 },
-  { id: 15, name: "Emperor", image: menu3 },
-  { id: 16, name: "Grouper (Hamour)", image: menu4 },
-  { id: 17, name: "Soldier Bream (Koffer)", image: menu5 },
-  { id: 18, name: "Yellowfin Tuna", image: menu6 },
-  { id: 19, name: "Lobster", image: menu7 },
-  { id: 20, name: "Ribbon Fish", image: menu8 },
-  { id: 21, name: "Sardines", image: menu9 },
-  { id: 22, name: "Shrimps", image: menu10 },
-  { id: 23, name: "Trevally", image: menu11 },
-  { id: 24, name: "Red Grouper (Disco)", image: menu12 },
+  { id: 1, name: "Yellow Fin Tuna", scientificName: "Thunnus Albacares", image: yellowfinImage },
+  { id: 2, name: "Kawa Kawa Tuna", scientificName: "Euthynnus Affinis", image: kawakawaImage },
+  { id: 3, name: "Bonito Tuna", scientificName: "Sarda Orientalis", image: bonitoImage },
+  { id: 4, name: "Emperor", scientificName: "Lethrinus Spp", image: emperorImage },
+  { id: 5, name: "Arabian Pandora", scientificName: "Pagellus Affinis", image: arabianPandoraImage },
+  { id: 6, name: "Red Seabream", scientificName: "Pagrus Pagrus", image: redSeabreamImage },
+  { id: 7, name: "Trevally", scientificName: "Caranx Spp", image: trevallyImage },
+  { id: 8, name: "Koffer", scientificName: "Evynnis Ehrenberji", image: kofferImage },
+  { id: 9, name: "Threadfin Bream", scientificName: "Nemipterus Japonicus", image: threadfinBreamImage },
+  { id: 10, name: "Blue Line Snapper", scientificName: "Lujanus Coerulineatus", image: blueLineSnapperImage },
 ];
 
 const MenuSection = () => {
@@ -33,8 +29,8 @@ const MenuSection = () => {
       <div className="menu-container">
         <h2 className="headline-1 text-center">Discover our products</h2>
         <ul className="grid-list">
-          {productData.map((item, index) => (
-            <li key={index} className="menu-card">
+          {productData.map((item) => (
+            <li key={item.id} className="menu-card">
               <figure className="menu-card-banner">
                 <img
                   src={item.image}
@@ -46,6 +42,7 @@ const MenuSection = () => {
                   <Link to="#" className="menu-card-title">
                     {item.name}
                   </Link>
+                  <p className="menu-card-scientific-name">{item.scientificName}</p>
                 </figcaption>
               </figure>
             </li>
